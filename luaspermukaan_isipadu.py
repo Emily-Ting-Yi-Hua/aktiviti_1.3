085f4d55b984bfb5cbb7eb3f3433c69a62c9913c
Emily-Ting-Yi-Hua <163367506+Emily-Ting-Yi-Hua@users.noreply.github.com>
2024-03-22 08:48:56 +0800

    Create luaspermukaan_isipadu.py 1

---
+++ luaspermukaan_isipadu.py
@@ -0,0 +1,7 @@
+jejari=float(input("Masukkan jejari:"))
+tinggi=float(input("Masukkan tinggi:"))
+pi=3.142
+luaspermukaan=(2*pi*jejari*tinggi)+(2*pi*jejari*jejari)
+isipadu=pi*jejari*jejari*tinggi
+print("Jumlah luas permukaaan ialah",luaspermukaan,"dan isi padu tangki air ialah",isipadu)
+
